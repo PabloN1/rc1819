236ac9f303275e1a076b5da1656f40daa060eae1
Nuno <nunong1995@gmail.com>
2019-03-06 14:03:52 +0100

    Persona.pl

---
+++ persona.pl
@@ -0,0 +1,18 @@
+
+% hombre(Persona)
+% Es cierto si Persona es un hombre.
+
+
+hombre(jose).
+hombre(manuel).
+hombre(antonio).
+
+
+%mujer(Persona)
+% Es cierto si Persona es una mujer.
+
+mujer(luisa).
+mujer(maria).
+mujer(fatima).
+
+
